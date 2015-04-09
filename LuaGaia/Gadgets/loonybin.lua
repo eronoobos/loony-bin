e79f42b9de7500d43addcc64844d5a7da30ef64e
eronoobos <eronoobos@gmail.com>
2015-04-09 11:51:15 -0700

    add metal and features

--- LuaGaia/Gadgets/loonybin.lua
+++ LuaGaia/Gadgets/loonybin.lua
@@ -17,6 +17,10 @@ local spSetHeightMapFunc	= Spring.SetHeightMapFunc
 local spSetHeightMap		= Spring.SetHeightMap
 local spAddHeightMap		= Spring.AddHeightMap 
 local spLevelHeightMap		= Spring.LevelHeightMap
+local spSetMetalAmount = Spring.SetMetalAmount
+local spCreateFeature = Spring.CreateFeature
+local spGetGroundHeight = Spring.GetGroundHeight
+
 
 
 ----- SPRING SYNCED ------------------------------------------
@@ -25,24 +29,39 @@ if (gadgetHandler:IsSyncedCode()) then
 
 local Loony = include "LoonyModule/loony.lua"
 local myWorld
+local heightRenderComplete, metalRenderComplete
 
 function gadget:Initialize()
-	myWorld = Loony.World(Game.mapX, Game.mapY, 4, 100)
+	myWorld = Loony.World(Game.mapX, Game.mapY, 5, 100)
 	myWorld.mirror = "rotational"
 	myWorld:MeteorShower(10)
 	myWorld:RenderHeight()
-end
-
-function gadget:GameFrame(frame)
-	myWorld:RendererFrame(frame)
+	myWorld:RenderMetal()
+	-- i have to change the height map here and not through GameFrame so that it happens before pathfinding & team LOS initialization
+	local i = 1
+	while not heightRenderComplete or not metalRenderComplete do
+		myWorld:RendererFrame(i)
+	end
+	local featureslist = myWorld:GetFeaturelist()
+	for i,fDef in pairs(featureslist) do
+		local stop = false
+		if fDef.name == "GeoVent" then
+			-- don't put geovents underwater
+			local y = spGetGroundHeight(fDef.x, fDef.z)
+			if y < 0 then stop = true end
+		end
+		if not stop then
+			local flagID = spCreateFeature(fDef.name, fDef.x, spGetGroundHeight(fDef.x,fDef.z)+5, fDef.z, fDef.rot)
+		end
+	end
 end
 
 -- Loony callins
 
 function Loony.CompleteRenderer(renderer)
+	local mapRuler = renderer.mapRuler
 	if renderer.renderType == "Height" then
 		-- write height map array to spring map when finished rendering
-		local mapRuler = renderer.mapRuler
 		local baselevel = renderer.world.baselevel
 		spSetHeightMapFunc(function()
 			for x, yy in pairs(renderer.data) do
@@ -52,6 +71,14 @@ function Loony.CompleteRenderer(renderer)
 				end
 			end
 		end)
+		heightRenderComplete = true
+	elseif renderer.renderType == "Metal" then
+		for x, yy in pairs(renderer.data) do
+			for y, mAmount in pairs(yy) do
+				spSetMetalAmount(x, y, mAmount)
+			end
+		end
+		metalRenderComplete = true
 	end
 end
 
